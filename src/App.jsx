c519cc060a0b41e4e6f0a389d33d17169ea97619
anjana732 <anjanasingh1257@gmail.com>
2024-03-10 19:08:02 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <>
+      <h1><center style={{color:'white'}}>To DO List</center></h1>
+    </>
+  )
+}
+
+export default App
